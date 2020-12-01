484bffa6a374ffa0ad9e8833e1210de4aa1a2800
Tyler Chase <tylercchase@gmail.com>
2020-11-30 19:44:19 -0900

    New code styling

--- coderun-web/src/app/new-code/new-code.component.ts
+++ coderun-web/src/app/new-code/new-code.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,8 +10,8 @@ import { Router } from '@angular/router';
 })
 export class NewCodeComponent implements OnInit {
   codeForm = new FormGroup({
-    title: new FormControl(''),
-    language: new FormControl('')
+    title: new FormControl('',[Validators.required]),
+    language: new FormControl('',[Validators.required])
   });
   constructor(private http: HttpClient,
     private router: Router) { }
