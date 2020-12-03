d2027cec7b882d3d37f91698bbfa5b4821dff126
Tyler Chase <tylercchase@gmail.com>
2020-12-02 22:58:36 -0900

    Change callback URL

--- coderun-backend/config/passport.js
+++ coderun-backend/config/passport.js
@@ -7,7 +7,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: '/api/auth/google/callback',
     },
 
     async (accessToken, refreshToken, profile, done) => {
